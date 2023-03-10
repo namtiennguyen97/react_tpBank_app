f4ab7036cf1681a5a0968a5a77280f92b4868d9a
namtiennguyen97 <toilanam97@gmail.com>
2023-03-10 11:34:38 +0700

    update

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,7 @@ function Home(){
     const navigate = useNavigate();
     const location = useLocation()
     const [token, setToken] = useState(location.state?.tp_token ?? localStorage.getItem('tp_token'))
-    const [userInfo, setUserInfo] = useState([])
+    const [userInfo, setUserInfo] = useState({})
 
     useEffect(()=> {
         //check auth
@@ -24,60 +24,57 @@ function Home(){
         axios.get(`${HOST}/api/get-account?token=${token}`)
             .then(res => {
                 if(res.data.code === statusCode.OK){
-                    setUserInfo(res.data.data)
+                    setUserInfo(res.data.data[0]) //get only first card type
                 } else{
-                    // localStorage.removeItem('tp_token')
-                    // navigate('/login')
-                    // toast.error(res.data.msg, {
-                    //     toastId: customERR
-                    // })
+                    localStorage.removeItem('tp_token')
+                    navigate('/login')
                 }
             })
     },[])
 
     return (
         <div className="container">
-            {userInfo.map((item, index) =>
-                (
-                    <div key={index}>
-                        <div className="card-outer">
-                            <div className="card">
-                                <span className="circle"></span>
-                                <div className="top-div">
-                                    <div className="chip-image">
-                                        <img src={chipIcon} />
-                                    </div>
-
-                                    <div className="plus-sign">
-                                        <span></span>
-                                        <span></span>
-                                    </div>
-                                </div>
-
-                                <div className="card-details">
-                                    <div className="card-number">
-                                        <span>{item.BBAN}</span>
-                                    </div>
-                                    <div className="date">
-                                        <span>Loại thẻ: </span>
-                                    </div>
-                                </div>
-                                <div className="ownername">
-
-                                    <span>{item.name}</span>
-                                    <img src="https://imgur.com/KpIC2Cs.png" />
-
-                                </div>
+            <div>
+                <div className="card-outer">
+                    <div className="card">
+                        <span className="circle"></span>
+                        <div className="top-div">
+                            <div className="chip-image">
+                                <img src={chipIcon} />
+                            </div>
+                            <div className="plus-sign">
+                                <span></span>
+                                <span></span>
                             </div>
                         </div>
-                        <div className="menu-board">
-                            adadawd
+
+                        <div className="card-details">
+                            <div className="card-number">
+                                <span>{userInfo.BBAN}</span>
+                            </div>
+                            <div className="date">
+                                <span>Loại thẻ: </span>
+                            </div>
+                        </div>
+                        <div className="ownername">
+
+                            <span>{userInfo.name}</span>
+                            <img src="https://imgur.com/KpIC2Cs.png" />
+
                         </div>
                     </div>
-                )
-            )}
-
-
+                </div>
+                <div className="menu-board">
+                    <div className="action-board">
+                        <button className="action-btn">
+                            Chuyển tiền
+                            <i className="fa fa-money"></i>
+                        </button>
+                        <button className="action-btn">Lịch sử <i className="fa-solid fa-money-check-pen"></i></button>
+                        <button className="action-btn">Cài đặt <i className="fa-solid fa-gear setting-icon"></i></button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
